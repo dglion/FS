26e3aba3688ec290fcb537926a102fdad3cfa3aa
dglion <35028377+dglion@users.noreply.github.com>
2018-09-17 12:06:04 +0300

    Update FS.class.php

--- FS.class.php
+++ FS.class.php
@@ -1,4 +1,45 @@
 <?php
 
 class FS
-{}
+{
+  public static function separator()
+  {
+    return DIRECTORY_SEPARATOR;
+  }
+  
+  public static function toNativeSeparators($pname)
+  {
+    return str_replace(array('\\', '/'), static::separator(), $pname);
+  }
+  
+  public static function rmDoubleSeparators($pname)
+  {
+    $doubleSeparators = static::separator() . static::separator();
+    
+    while (strpos($pname, $doubleSeparators) !== false) {
+      $pname = str_replace($doubleSeparators, static::separator(), $pname);
+    }
+    
+    return $pname;
+  }
+  
+  public static function rmTrailingSeparator($pname)
+  {
+    return rtrim($pname, static::separator());
+  }
+  
+  public static function checkTrailingSeparator($pname)
+  {
+    return static::rmTrailingSeparator($pname) . static::separator();
+  }
+  
+  public static function rmLeadingSeparator($pname)
+  {
+    return ltrim($pname, static::separator());
+  }
+  
+  public static function checkLeadingSeparator($pname)
+  {
+    return static::separator() . static::rmLeadingSeparator($pname);
+  }
+}
